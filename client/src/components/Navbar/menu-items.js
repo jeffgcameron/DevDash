f32b90d72c030c0253b6c5dd34e72907359e2712
Brendan Bennett <bennettbrendan598@gmail.com>
2021-06-02 17:15:34 -0600

    Navbar MVP done..still working on styling for it

---
+++ client/src/components/Navbar/menu-items.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        icon: <span class="icon"><i class="fas fa-home"></i></span>,
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links',
+        
+    },
+    {
+        icon: <span class="icon"><i class="fas fa-info-circle"></i></span>,
+        title: 'About',
+        url: '#',
+        cName: 'nav-links',
+        
+    },
+    {
+        icon: <span class="icon"><i class="fas fa-envelope"></i></span>,
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links',
+        
+    },
+]
