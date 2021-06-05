d234c976cee2286e6ad336fad3a7962cdb0c5c53
Kelsey Benedict <kelseybenedict26@gmail.com>
2021-06-04 21:38:02 -0600

    created message button component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar/navbar"
 import Logo from "./components/DevDash-Logo/devlogo"
+// import Profile from "./pages/Profile";
+// import MessageIcon from "./components/Message-Icon/messageIcon"
 
 function App() {
   return (
---
+++ client/src/components/Message-Icon/messageIcon.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+}));
+
+
+function MessageIcon() {
+
+    function handleMessageSubmit(e){
+        e.preventDefault()
+        //console.log("clicked");
+    }
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <IconButton onClick={handleMessageSubmit}>
+                <MailOutlineIcon />
+            </IconButton>
+        </div>
+    )
+}
+export default MessageIcon;
