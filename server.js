5c19125b3ca0ff1f331256a1f440c0b766998c9c
Jeff Cameron <jeffgcameron@gmail.com>
2021-06-12 14:23:45 -0600

    heroku3

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../dist", "index.html"))
+    res.sendFile(path.join(__dirname, "../build", "index.html"))
   })
 }
 // Add routes, both API and view
