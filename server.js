0224b2f5d34e62430cb7d62acb398daa34821a36
Jeff Cameron <jeffgcameron@gmail.com>
2021-06-12 13:56:46 -0600

    heroku2

--- server.js
+++ server.js
@@ -41,6 +41,9 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../dist", "index.html"))
+  })
 }
 // Add routes, both API and view
 app.use(routes);
