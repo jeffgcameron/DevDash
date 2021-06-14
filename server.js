a323b5b693b772c7f168fd63d6dfce2ef656ff79
Jeff Cameron <jeffgcameron@gmail.com>
2021-06-13 20:24:55 -0600

    heroku commit

--- server.js
+++ server.js
@@ -39,12 +39,25 @@ app.use(session(sess))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", function (req, res) {
+//     res.sendFile(path.join(__dirname, "../build", "index.html"))
+//   })
+// }
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  // Handle React routing, return all requests to React app
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../build", "index.html"))
-  })
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+  });
 }
+
+
+
 // Add routes, both API and view
 app.use(routes);
 app.use("/api", apiRoutes);
